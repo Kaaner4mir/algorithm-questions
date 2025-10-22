c411da87a460175d796605a84e662a6f2d2e099e
Kaaner <kaaner4mir@gmail.com>
2025-10-22 08:47:34 +0300

    add EasyLevel_1-5

---
+++ EasyLevel_1-5/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.ComponentModel;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    public static void Main()
+    {
+        #region 1. Question
+
+        ////Read a number from the user and print the numbers from 1 up to that number.
+
+        //Console.Write("Please enter a positive integer: ");
+        //int number=Convert.ToInt32(Console.ReadLine());
+
+        //for (int i = number; i > 0; i--)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        #endregion
+
+        #region 2. Question
+
+        ////Write a program that prints even numbers from 1 to 100.
+
+        //for (int i = 2; i <= 100; i=i+2)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        #endregion
+
+        #region 3. Question
+
+        ////Write a program that determines whether an entered number is prime.
+
+        //Console.Write("Enter the integer you want to learn factorial value: ");
+        //int number = Convert.ToInt32(Console.ReadLine());
+
+        //bool isPrime = true;
+
+        //for (int i = 2; i <= Math.Sqrt(number); i++)
+        //{
+        //    if (number % i == 0)
+        //    {
+        //        isPrime = false;
+        //    }
+        //}
+
+        //if (isPrime)
+        //    Console.WriteLine($"{number} is a prime number.");
+        //else
+        //    Console.WriteLine($"{number} is not a prime number.");
+        #endregion
+
+        #region 4. Question
+
+        //// Read a number from the user and calculate its factorial.
+
+        //Console.Write("Please enter an positive integer you want to learn factorial value: ");
+        //long number = Convert.ToInt64(Console.ReadLine());
+
+        //long result = 1;
+
+        //for (long i = number; i >=1; i--)
+        //{
+        //    result *= i;
+        //}
+
+        //Console.WriteLine($"{number}! = {result}");
+
+        #endregion
+
+        #region 5. Question
+
+        // Write a program that finds the sum of odd numbers between 1 and 50.
+
+        int result = 0;
+
+        for (int i = 1; i <= 50; i = i + 2)
+        {
+            result += i;
+        }
+
+        Console.WriteLine(result);
+
+        #endregion
+
+        Console.ReadLine();
+
+    }
+}
