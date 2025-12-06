f54c539e92dbf01c3f10d7efc7009a9c6ee25a99
Emirhan Kaaner <kaaner4mir@gmail.com>
2025-12-06 18:38:50 +0300

    answer five questions

---
+++ Basic1_10/Program.cs
@@ -0,0 +1,73 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // 1. Print the sum of the two entered numbers on the screen.
+
+        //Console.Write("Enter first number: ");
+        //double number1=Convert.ToDouble(Console.ReadLine());
+
+        //Console.Write("Enter second number: ");
+        //double number2=Convert.ToDouble(Console.ReadLine());
+
+        //double sum=number1+number2;
+
+        //Console.WriteLine($"{number1} + {number2} = {sum}");
+
+        // 2. Check whether the entered number is odd or even.
+
+        //Console.Write("Enter the number you want to check whether odd or not: ");
+        //double number=Convert.ToDouble(Console.ReadLine());
+
+        //if(number%2== 0)
+        //    Console.WriteLine($"{number} is an even number.");
+        //else
+        //    Console.WriteLine($"{number} is an odd number.");
+
+        // 3. Find the sum of numbers from 1 to 100.
+
+        //int sum = 0;
+
+        //for (int i = 0; i < 100; i++)
+        //{
+        //    sum += i;
+        //}
+
+        //Console.WriteLine($"The sum of numbers from 1 to 100 is: {sum}");
+
+        // 4. Calculate the factorial of the entered number.
+
+        //Console.Write("Enter the number you want to calculate the factorial of: ");
+        //int number = Convert.ToInt32(Console.ReadLine());
+
+        //int result = 1;
+
+        //for(int i = number; i>0; i--)
+        //{
+        //    result *= i;
+        //}
+
+        //Console.WriteLine($"The factorial of {number} is: {result}");
+
+        // 5. Check whether the entered number is prime.
+
+        //Console.Write("Enter the number you want to check whether prime or not: ");
+        //int number = Convert.ToInt32(Console.ReadLine());
+
+        //bool isPrime = true;
+
+        //for (int i = 2; i <= Math.Sqrt(number); i++)
+        //{
+        //    if (number % i == 0)
+        //    {
+        //        isPrime = false;
+        //        break;
+        //    }
+        //}
+
+        //if (isPrime)
+        //    Console.WriteLine($"{number} is a prime number.");
+        //else
+        //    Console.WriteLine($"{number} is not a prime number.");
+    }
+}
